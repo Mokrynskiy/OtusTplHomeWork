e41631cfb4e914f36b7e8a7f3bd697dc312f8b89
Александр Мокринский <mokrynskiy@gmail.com>
2024-03-01 10:51:09 +0700

    Добавьте файлы проекта.

---
+++ OtusTplHomeWork/ParalelFileReader.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+
+namespace OtusTplHomeWork
+{
+    public class ParalelFileReader
+    {
+        public delegate void ReadComplit(ParalelFileReader sender, ReadComplitEventArgs e);
+        public event ReadComplit? ReadComplitEventHandler;
+
+        public void ReadAllFromDirectory(string folderPath)
+        {
+            if(Directory.Exists(folderPath))
+            {
+                ReadFromFilePathList(Directory.GetFiles(folderPath).ToList());
+            }
+        }
+        public void ReadFromFilePathList(List<string> filePathList)
+        {
+            foreach (string filePath in filePathList)
+            {
+                if (File.Exists(filePath))
+                {
+                    Task.Run(() => ReadSpaceCountFromFile(filePath));
+                }
+            }
+        }
+        private void ReadSpaceCountFromFile(string filePath)
+        {
+            long spaceCount = 0;
+            try
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                using(var sr = new StreamReader(filePath))
+                {
+                    foreach (var item in sr.ReadToEnd())
+                    {
+                        if (item == ' ') spaceCount++;                        
+                    }
+                }
+                sw.Stop();
+                ReadComplitEventHandler?.Invoke(this, new ReadComplitEventArgs { FilePath = filePath, SpaceCount = spaceCount, Time = sw.Elapsed });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+    
+    public class ReadComplitEventArgs: EventArgs
+    {
+        public string FilePath { get; set; }
+        public long SpaceCount { get; set; }
+        public TimeSpan Time { get; set; }
+    }
+}
---
+++ OtusTplHomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OtusTplHomeWork
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {            
+            //List<string> filePathList = new List<string> { "Txt\\Book1.txt", "Txt\\Book2.txt", "Txt\\Book3.txt" };
+            ParalelFileReader reader = new ParalelFileReader();
+            reader.ReadComplitEventHandler += DisplayReadFileResult;
+            
+            //reader.ReadFromFilePathList(filePathList);
+            
+            reader.ReadAllFromDirectory("Txt");
+            Console.ReadKey();
+        }
+        static void DisplayReadFileResult(ParalelFileReader sender, ReadComplitEventArgs e)
+        {
+            Console.WriteLine($"Фаил {Path.GetFileName(e.FilePath)} прочитан. Количество пробелов {e.SpaceCount}. Время чтения {e.Time}");
+        }
+    }
+}
